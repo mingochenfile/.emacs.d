ef628364bd7af5d6f31a9cbe3f0e3ee127fb3766
Ming Chen <mingche@amazon.com>
2014-06-18 17:23:21 -0700

    new'

--- init.el
+++ init.el
@@ -1121,7 +1121,9 @@ search-term
 
 ;; To install magit
 (add-to-list 'load-path "~/.emacs.d/magit/")
+(add-to-list 'load-path "~/.emacs.d/git-modes/")
 (eval-after-load 'info
   '(progn (info-initialize)
 	  (add-to-list 'Info-directory-list "~/.emacs.d/magit/")))
 (require 'magit)
+;; (require 'git-commit-mode)
